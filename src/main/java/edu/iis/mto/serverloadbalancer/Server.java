d5d24aca654e541f61d01a1165987363468778bc
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-06-11 14:24:05 +0200

    test5 green light. After refactoring.

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -7,7 +7,7 @@ import java.util.List;
  * Created by Wojciech Szczepaniak on 11.06.2017.
  */
 public class Server {
-    private final double MAXIMUM_LOAD = 100.0d;
+    public static final double MAXIMUM_LOAD = 100.0d;
     public double currentLoadPercentage;
     public int capacity;
 
--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -6,13 +6,22 @@ package edu.iis.mto.serverloadbalancer;
 public class ServerLoadBalancer {
     public void balance(Server[] servers, Vm[] vms) {
         for (Vm vm : vms) {
-            Server lessLoadedServer = null;
-            for (Server server : servers) {
-                if (lessLoadedServer == null || server.currentLoadPercentage < lessLoadedServer.currentLoadPercentage) {
-                    lessLoadedServer = server;
-                }
-            }
-            lessLoadedServer.addVm(vm);
+            addToLessLoadedServer(servers, vm);
         }
+    }
+
+    private void addToLessLoadedServer(Server[] servers, Vm vm) {
+        Server lessLoadedServer = findLessLoadedServer(servers);
+        lessLoadedServer.addVm(vm);
+    }
+
+    private Server findLessLoadedServer(Server[] servers) {
+        Server lessLoadedServer = null;
+        for (Server server : servers) {
+            if (lessLoadedServer == null || server.currentLoadPercentage < lessLoadedServer.currentLoadPercentage) {
+                lessLoadedServer = server;
+            }
+        }
+        return lessLoadedServer;
     }
 }
